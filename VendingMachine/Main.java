f107cb26238f429867465673d37ac7f89863cdaf
Лилия <l_loshakova123@mail.ru>
2023-01-03 18:28:00 +0300

    Sketch

--- VendingMachine/Main.java
+++ VendingMachine/Main.java
@@ -1,5 +1,5 @@
 public class Main {
-    public static void main(String[] args) {
-        VendingMachine vendingMachine = new VendingMachine();
-    }
+    // public static void main(String[] args) {
+    //     VendingMachine vendingMachine = new VendingMachine();
+    // }
 }
--- VendingMachine/VendingMachine.java
+++ VendingMachine/VendingMachine.java
@@ -1,5 +1,16 @@
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class VendingMachine {
     List<Product> products;
+
+
+    public VendingMachine(List<Product> products){
+        this.products = products;
+    }
+    
+    public VendingMachine(){
+        this(new ArrayList<>());
+    }
 }
